c1e1c103ddadc1bc1916e20a1e66fd335541ac09
justin rey <justinrey312@gmail.com>
2026-05-18 22:04:07 -0800

    adjusting the app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ except ImportError:
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 
